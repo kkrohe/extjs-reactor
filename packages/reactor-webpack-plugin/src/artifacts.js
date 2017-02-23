a44ba9d83dbe10fac39392bd9370d31c7a6a3443
Kevin Krohe <kevin.krohe@sencha.com>
2017-02-23 12:45:46 -0600

    upgrade to fashion watch

--- packages/reactor-webpack-plugin/src/artifacts.js
+++ packages/reactor-webpack-plugin/src/artifacts.js
@@ -19,93 +19,102 @@ export const buildXML = function({ compress }) {
     }
 
     return `
-        <project name="simple-build" basedir=".">
-            <!--  internally, watch calls the init target, so need to have one here -->
-            <target name="init"/>
-            <target name="init-cmd">
-                <taskdef resource="com/sencha/ant/antlib.xml"
-                                classpath="\${cmd.dir}/sencha.jar"
-                                loaderref="senchaloader"/>
-                <x-extend-classpath>
-                    <jar path="\${cmd.dir}/sencha.jar"/>
-                </x-extend-classpath>
-                <x-sencha-init prefix=""/>
-                <x-compile refid="theCompiler"
-                                 dir="\${basedir}"
-                                 initOnly="true"
-                                 inheritAll="true">
-                          <![CDATA[
-                          -classpath=\${basedir}/manifest.js
-                          load-app
-                              -temp=\${basedir}/temp
-                              -tag=App
-                    ]]>
-                  </x-compile>
-            </target>
-            <target name="build" depends="init-cmd">
-               <x-compile refid="theCompiler"
-                          dir="\${basedir}"
-                          inheritAll="true">
-                  <![CDATA[
-                  exclude
-                  -all
-                  and
-                  include
-                  -f=Boot.js
-                  and
-                  concatenate
-                      ext.js
-                  and
-                  exclude
-                  -all
-                  and
-                  # include all js files needed for manifest.js
-                      include
-                      -r
-                      -f=manifest.js
-                  and
-                  # exclude the generated manifest file itself,
-                  # since we don't want the generated bundle file to create any components
-                  exclude
-                  -f=manifest.js
-                  and
-                  concatenate
-                  +append
-                      ext.js
-                  and
-                  scss
-                      -appName=App
-                      -imageSearchPath=resources
-                      -themeName=triton
-                      -resourceMapBase=.
-                      -output=ext.scss
-                  and
-                  resources
-                      -excludes=-all*.css
-                      -out=resources
-                  and
-                  resources
-                      -model=true
-                      -out=resources
-                  ]]>
-               </x-compile>
-               <x-sencha-command dir="\${basedir}">
-                   <![CDATA[
-                   fashion
-                       -pwd=.
-                       -split=4095
-                       ${compress ? '-compress' : ''}
-                           ext.scss
-                       ext.css
-                   ${compression}
-                   ]]>
-               </x-sencha-command>
-            </target>
-            <target name="watch" depends="init-cmd,build">
-                <x-watch compilerRef="theCompiler"
-                         targets="build"/>
-            </target>
-        </project>
+<project name="simple-build" basedir=".">
+    <!--  internally, watch calls the init target, so need to have one here -->
+    <target name="init"/>
+    <target name="init-cmd">
+        <taskdef resource="com/sencha/ant/antlib.xml"
+                 classpath="\${cmd.dir}/sencha.jar"
+                 loaderref="senchaloader"/>
+        <x-extend-classpath>
+            <jar path="\${cmd.dir}/sencha.jar"/>
+        </x-extend-classpath>
+        <x-sencha-init prefix=""/>
+        <x-compile refid="theCompiler"
+                   dir="\${basedir}"
+                   initOnly="true"
+                   inheritAll="true">
+            <![CDATA[
+                  -classpath=\${basedir}/manifest.js
+                  load-app
+                      -temp=\${basedir}/temp
+                      -tag=App
+            ]]>
+        </x-compile>
+    </target>
+    <target name="build" depends="init-cmd">
+        <x-compile refid="theCompiler"
+                   dir="\${basedir}"
+                   inheritAll="true">
+            <![CDATA[
+                exclude
+                    -all
+                and
+                include
+                    -f=Boot.js
+                and
+                concatenate
+                    ext.js
+                and
+                exclude
+                    -all
+                and
+                # include all js files needed for manifest.js
+                include
+                    -r
+                    -f=manifest.js
+                and
+                # exclude the generated manifest file itself,
+                # since we don't want the generated bundle file to create any components
+                exclude
+                    -f=manifest.js
+                and
+                concatenate
+                    +append
+                    ext.js
+                and
+                scss
+                    +skipWatch
+                    -appName=App
+                    -imageSearchPath=resources
+                    -themeName=triton
+                    -resourceMapBase=.
+                    -output=ext.scss
+                and
+                resources
+                    -excludes=-all*.css
+                    -out=resources
+                and
+                resources
+                    -model=true
+                    -out=resources
+            ]]>
+        </x-compile>
+        <x-sencha-command dir="\${basedir}">
+            <![CDATA[
+                fashion
+                    -pwd=.
+                    -split=4095
+                    ${compress ? '-compress' : ''}
+                    ext.scss
+                    ext.css
+                    ${compression}
+            ]]>
+        </x-sencha-command>
+    </target>
+    <target name="watch" depends="init-cmd,build">
+        <x-fashion-watch
+                refName="fashion-watch"
+                inputFile="ext.scss"
+                outputFile="ext.css"
+                split="4095"
+                compress="${compression ? 'true' : 'false'}"
+                configFile="\${app.dir}/app.json"
+                fork="true"/>
+        <x-watch compilerRef="theCompiler"
+                 targets="build"/>
+    </target>
+</project>
     `.trim();
 };
 
